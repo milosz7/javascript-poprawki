b21690fca36561c9f1ae9001b3643f04b174a06f
Milosz Bargiel <miloszbargiel@gmail.com>
2022-03-07 14:34:27 +0100

    Add buttons to control the game

--- js/script.js
+++ js/script.js
@@ -1,40 +1,52 @@
-let moveRandomiser = Math.floor(Math.random() * 3 + 1);
+function playGame(playerInput) {
+  clearMessages();
 
-const computerMove = getMoveName(moveRandomiser);
+  let moveRandomiser = Math.floor(Math.random() * 3 + 1);
 
-function getMoveName(moveId) {
-  moveId = parseInt(moveId);
-  if (moveId === 1) {
-    return 'rock';
-  } else if (moveId === 2) {
-    return 'paper';
-  } else if (moveId === 3) {
-    return 'scissors';
+  const computerMove = getMoveName(moveRandomiser);
+
+  function getMoveName(moveId) {
+    moveId = parseInt(moveId);
+    if (moveId === 1) {
+      return 'rock';
+    } else if (moveId === 2) {
+      return 'paper';
+    } else if (moveId === 3) {
+      return 'scissors';
+    }
+    printMessage(playerInput + ' is an unknown move ID.');
+    return 'an unknown move';
   }
-  printMessage(playerInput + ' is an unknown move ID.');
-  return 'an unknown move';
+
+  let playerMove = getMoveName(playerInput);
+
+  function displayResult(computerMove, playerMove) {
+    printMessage('I played ' + computerMove + ', and you played ' + playerMove + '.');
+
+    if (playerInput > 3) {
+      printMessage('Enter a valid move.');
+    } else if (computerMove === playerMove) {
+      printMessage("It's a draw!");
+    } else if (
+      (computerMove === 'rock' && playerMove === 'paper') ||
+      (computerMove === 'scissors' && playerMove === 'rock') ||
+      (computerMove === 'paper' && playerMove === 'scissors')
+    ) {
+      printMessage('You win!');
+    } else {
+      printMessage('You lose!');
+    }
+  }
+
+  displayResult(computerMove, playerMove);
 }
 
-let playerInput = prompt('Enter your move! 1: rock, 2: paper, 3: scissors.');
-
-let playerMove = getMoveName(playerInput);
-
-function displayResult(computerMove, playerMove) {
-  printMessage('I played ' + computerMove + ', and you played ' + playerMove + '.');
-
-  if (playerInput > 3) {
-    printMessage('Enter a valid move.');
-  } else if (computerMove === playerMove) {
-    printMessage("It's a draw!");
-  } else if (
-    (computerMove === 'rock' && playerMove === 'paper') ||
-    (computerMove === 'scissors' && playerMove === 'rock') ||
-    (computerMove === 'paper' && playerMove === 'scissors')
-  ) {
-    printMessage('You win!');
-  } else {
-    printMessage('You lose!');
-  }
-}
-
-displayResult(computerMove, playerMove);
+document.getElementById('playRock').addEventListener('click', function () {
+  playGame(1);
+});
+document.getElementById('playPaper').addEventListener('click', function () {
+  playGame(2);
+});
+document.getElementById('playScissors').addEventListener('click', function () {
+  playGame(3);
+});
