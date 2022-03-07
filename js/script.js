22eca38759879498a917d1615a386e9565b8192b
Milosz Bargiel <miloszbargiel@gmail.com>
2022-03-07 12:23:19 +0100

    Add players moves generation and final score loop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
-let computerMove = 'kamień';
+let moveRandomiser = Math.floor(Math.random() * 3 + 1);
 
-let playerMove = 'papier';
+let computerMove = 'unknown';
 
-printMessage('Zagrałem ' + computerMove + '. Jeśli twój ruch to ' + playerMove + ' to wygrywasz!');
+if (moveRandomiser === 1) {
+  computerMove = 'rock';
+} else if (moveRandomiser === 2) {
+  computerMove = 'paper';
+} else {
+  computerMove = 'scissors';
+}
+
+let playerMove = 'unknown';
+
+let playerInput = prompt('Enter your move! 1: rock, 2: paper, 3: scissors.');
+
+if (playerInput === '1') {
+  playerMove = 'rock';
+} else if (playerInput === '2') {
+  playerMove = 'paper';
+} else if (playerInput === '3') {
+  playerMove = 'scissors';
+}
+
+printMessage('I played ' + computerMove + ', and you played ' + playerMove + '.');
+
+if (computerMove === playerMove) {
+  printMessage("It's a draw!");
+} else if (computerMove === 'rock' && playerMove === 'scissors') {
+  printMessage('You lose!');
+} else if (computerMove === 'scissors' && playerMove === 'paper') {
+  printMessage('You lose!');
+} else if (computerMove === 'paper' && playerMove === 'rock') {
+  printMessage('You lose!');
+} else if (computerMove === 'scissors' && playerMove === 'rock') {
+  printMessage('You win!');
+} else if (computerMove === 'paper' && playerMove === 'scissors') {
+  printMessage('You win!');
+} else {
+  printMessage('This is not a valid move!');
+}
